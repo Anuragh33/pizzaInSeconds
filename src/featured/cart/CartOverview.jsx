61b6d13967587c88b397a39599c18ed35cb2c866
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2024-02-22 23:24:46 -0600

    uptd

--- src/featured/cart/CartOverview.jsx
+++ src/featured/cart/CartOverview.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className='bg-neutral-400 text-zinc-700'>
+      <p className='text-zinc-800'>
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
-      <Link to="/cart">Open cart &rarr;</Link>
+      <Link to='/cart'>Open cart &rarr;</Link>
     </div>
-  )
+  );
 }
 
-export default CartOverview
+export default CartOverview;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,13 @@
-import { Link } from "react-router-dom"
-import SearchOrder from "../featured/order/SearchOrder"
+import { Link } from 'react-router-dom';
+import SearchOrder from '../featured/order/SearchOrder';
 function Header() {
   return (
-    <header>
-      <Link to="/">Pizza In Seconds Co.</Link>
+    <header className='bg-emerald-500'>
+      <Link to='/'>Pizza In Seconds Co.</Link>
       <SearchOrder />
       <p>Anuragh</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
